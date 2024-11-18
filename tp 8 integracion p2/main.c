2501f2e99d29571a1fe5dd9e77d53f06939aed52
me <franconunez500@gmail.com>
2024-11-18 13:13:39 -0300

    add second exam

---
+++ tp 8 integracion p2/main.c
@@ -0,0 +1,30 @@
+
+// Una empresa desea procesar las ventas que efectúa conociendo los siguientes datos:
+
+// número de factura
+// código de articulo
+// cantidad vendida
+// precio unitario
+
+
+// En cada factura sólo se vende un tipo de artículo, los artículos son 6 y los datos finalizan con número de factura = 0              Se pide :
+
+
+
+// A.     cantidad de facturas emitidas
+
+// B.     monto de cada factura
+
+// C.     suma de las facturas (caja diaria)
+
+// D.     cantidad total de artículos vendidos
+
+// E.      cantidad vendida de cada artículo
+
+// F.      cantidad de facturas emitidas para cada artículo
+
+// G.     promedio de importe de las facturas
+
+// H.     porcentual que representa el monto total de cada artículo sobre el total  
+
+// I. saber si se vendió el artículo 4 (utilizar algortimo de búsqueda binaria).
